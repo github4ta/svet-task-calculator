2d82663dfd48696d23cecac9c650f986d2528148
Sergei Tsarik <serge.tsarik@gmail.com>
2023-04-25 19:11:19 +0300

    refactor code

--- src/Run.java
+++ src/Run.java
@@ -3,17 +3,7 @@ import java.util.Scanner;
 public class Run {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        StringBuffer sb = new StringBuffer();
-        String tmp;
-        while (scanner.hasNextLine()) {
-            tmp = scanner.nextLine();
-            if (tmp.isEmpty()) {
-                break;
-            }
-            sb.append(tmp);
-        }
-
-        String input = sb.toString();
+        String input = scanner.nextLine();
 
         String result = Main.calc(input);
         System.out.println(result);
--- src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) throws Exception {
-        String str = "1 + 2 + 3";
-        String result = Main.calc(str);
-
-        System.out.println(result);
-    }
-}
